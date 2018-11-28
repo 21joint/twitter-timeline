14e9c3f0a94ab7e89a4704a44538305de65f533a
prod1992 <levon1.grigoryan@gmail.com>
2018-11-28 08:25:10 +0400

    added docs folder

---
+++ docs/precache-manifest.6a17257f6148a05146ebe915601992cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14520a0a4ef572f1d5a6",
+    "url": "/static/js/main.14520a0a.chunk.js"
+  },
+  {
+    "revision": "02ec2595d731b96f7d99",
+    "url": "/static/js/1.02ec2595.chunk.js"
+  },
+  {
+    "revision": "14520a0a4ef572f1d5a6",
+    "url": "/static/css/main.352d07dc.chunk.css"
+  },
+  {
+    "revision": "0ab51d89c1ef75aa84f08601b9067c0d",
+    "url": "/index.html"
+  }
+];
