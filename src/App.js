9794e07a8984d5a19af88804ee28198cd0357c7d
prod1992 <levon1.grigoryan@gmail.com>
2018-11-28 07:42:27 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from "./components/Header"
+import {Timeline} from "react-twitter-widgets";
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            screen_name: ''
+        }
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            username: e.target.username.value,
+            screen_name: e.target.screen_name.value
+        });
+    }
+
+
+    render() {
+        const _timeline = (this.state.username && this.state.screen_name) ? <Timeline
+            dataSource={{
+                sourceType: 'profile',
+                screenName: this.state.screen_name
+            }}
+            options={{
+                username: this.state.username,
+                height: '100%',
+                width: '100%'
+            }}
+            onLoad={() => console.log('Timeline is loaded!')}
+        /> : "";
+        return (
+            <div className="App">
+                <Header
+                    username={this.state.username}
+                    screen_name={this.state.screen_name}
+                    onSubmit={this.onSubmit.bind(this)}/>
+                {_timeline}
+            </div>
+        );
+    }
 }
 
 export default App;
