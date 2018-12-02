de4ed275a4605a8eaaf540070e87871a5587370f
harutyunyansamo <samo.harutyunyan777@gmail.com>
2018-12-03 03:31:25 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React, {Component} from 'react';
+import Twitter from "twitter";
 import Timeline from "./Timeline";
 import './App.scss';
 
-class App extends Component {
-    state = {
-        screen_name: '',
-        tweets: []
-    }
+ 
+const client = new Twitter({
+    consumer_key: 'CL7qfSvWHGqPb8KAo4I9xgPIu',
+    consumer_secret: 'rPZc7NfGggwXumao3oXDNJIOVivwsudceXEFUblTeN87UjF7C1',
+    access_token_key: '962475972233711617-HoYApUK09U7QIv2lonDzcXzOfw7tzzD',
+    access_token_secret: '1isfTDNHwxY8Fy2XqUuyBSvf1xF4H3sAxdKZeyhfMdxKO'
+  });
 
-    onSubmit = (e) => {
-        e.preventDefault();
-        this.setState({
-            screen_name: e.target.screen_name.value
-        });
-    }
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            screen_name: '',
+            tweets: []
+        }
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({screen_name: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.screen_name);
+        event.preventDefault();
+        const params = {screen_name: this.state.screen_name};
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            console.log(response)
+            if (!error) {
+                console.log(tweets)
+              this.setState({
+                  tweets
+              })
+            }
+            else {
+                console.log(error)
+            }
+          });
+      }
 
 
     render() {
@@ -23,11 +54,15 @@ class App extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-12">
-                            <form onSubmit={this.props.onSubmit}>
+                            <form onSubmit={this.handleSubmit}>
                                 <div className="form-row">
                                     <div className="col">
-                                        <input type="text" className="form-control" name="screen_name"
-                                               placeholder="Screen Name"/>
+                                        <input type="text" 
+                                        className="form-control" 
+                                        name="screen_name"
+                                        placeholder="Screen Name" 
+                                        onChange={this.handleChange}
+                                        value={this.state.screen_name}/>
                                     </div>
                                     <div className="col-auto">
                                         <button className="btn btn-primary btn-block">Get Timeline</button>
@@ -39,7 +74,7 @@ class App extends Component {
                     </div>
                 </div>
             </header>
-                {this.state.tweets && <Timeline tweets={this.state.tweets}/>}
+            {this.state.tweets && <Timeline tweets={this.state.tweets}/>}
             </div>
         );
     }
